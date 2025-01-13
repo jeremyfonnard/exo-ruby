69624828fd3fa4d660738c89297604c1b8bd57ed
Jeremy Fonnard <jeremfrd@gmail.com>
2025-01-13 16:07:53 +0400

    Create exo8.rb

---
+++ exo8.rb
@@ -0,0 +1,7 @@
+puts "Bonjour, c'est quoi ton blase ?"
+print ">"
+user_name = gets.chomp
+puts user_name
+
+#get.chomp nous permet de d'entre un nom ici 
+# la différence entre les 3 programmes sont que le premier affiche seulement le message alors que les é autres nous permets d'entre une information dans le porgramme et de la valider. 
